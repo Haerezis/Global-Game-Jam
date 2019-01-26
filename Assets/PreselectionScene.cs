bfb02fc0d03f7e9d38e02b19eae3ba95d2996ec0
unknown <daniel.herrenschneider@gmail.com>
2019-01-26 22:30:19 +0100

    Ajout de la scene SelectionLevelScene avec des scripts de chargement

---
+++ Assets/PreselectionScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreselectionScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string lastLevel = null;
+        using (Stream stream = File.Open("Assets/save.txt", FileMode.Open, FileAccess.Read))
+        using (TextReader reader = new StreamReader(stream, Encoding.UTF8))
+        {
+            lastLevel = reader.ReadLine();
+        }
+
+        if (lastLevel != null)
+        {
+            if (lastLevel == "lvl01_Squirrel")
+            {
+                transform.position = new Vector2(-1, -1);
+            }
+            //si il était à côté, il pop par défaut à côté du bouton Tuto
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ReturnToLastScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToLastScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            /*using (Stream stream = File.Open("Assets/save.txt", FileMode.Truncate, FileAccess.Write))
+            using (TextWriter wr = new StreamWriter(stream, Encoding.UTF8))
+            {
+                wr.Write("MainMenuScene");*/
+            SceneManager.LoadScene(@"Scenes/MainMenuScene", LoadSceneMode.Single);
+            //changer la commande pour aller à la scène précédente
+            Debug.Log("Tuto launched");
+            //}
+        }
+    }
+}
