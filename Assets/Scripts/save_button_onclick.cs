c6ff494b6841a30ea4cced0ad4d029ac20637d8f
Kentigern <kentigern.hartmann@ensiie.fr>
2019-01-26 08:56:45 +0100

    Adding Settings menu, with a volume setting in order to play background music

---
+++ Assets/Scripts/save_button_onclick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class save_button_onclick : MonoBehaviour
+{
+
+    public Button save;
+    public Slider musicSlider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        save.onClick.AddListener(SaveOnClick);
+    }
+
+    // Update is called once per frame
+    void SaveOnClick()
+    {
+        using (Stream stream = File.Open("Assets/config.cfg.txt", FileMode.Truncate, FileAccess.Write))
+        using (TextWriter wr = new StreamWriter(stream, Encoding.UTF8))
+        {
+            wr.Write(musicSlider.value);
+            Debug.Log("Volume set");
+        }
+        SceneManager.LoadScene("OptionScene");
+    }
+}
---
+++ Assets/Scripts/set_music_volume.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class set_music_volume : MonoBehaviour
+{    
+    // Start is called before the first frame update
+    void Start()
+    {
+        using (Stream stream = File.Open("Assets/config.cfg.txt", FileMode.Open))
+        using (TextReader sr = new StreamReader(stream, Encoding.UTF8))
+        {
+            string volumeLevel;
+            if ((volumeLevel = sr.ReadLine()) != null)
+            {
+                this.gameObject.GetComponent<AudioSource>().volume = float.Parse(volumeLevel) / 100;
+            }
+        }
+    }
+}
