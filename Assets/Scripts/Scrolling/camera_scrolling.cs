c40c02982860b94c34d3346d57988cc0541e7251
Kentigern <kentigern.hartmann@ensiie.fr>
2019-01-26 17:18:23 +0100

    Implementing a first version of Camera and Water Scrolling

---
+++ Assets/Scripts/Scrolling/camera_scrolling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_scrolling : MonoBehaviour
+{
+    public float cameraOffSet;
+
+    private GameObject character;
+    private float maxPlayerHigh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxPlayerHigh = this.gameObject.transform.position.y;
+        character = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float currentPlayerHigh = character.transform.position.y - cameraOffSet;
+        if (currentPlayerHigh > maxPlayerHigh)
+        {
+            maxPlayerHigh = currentPlayerHigh;
+        }
+
+        // Trick allowing to modify current gameObject transform state
+        Vector3 camPosCopy = this.gameObject.transform.position;
+        camPosCopy.y = maxPlayerHigh;
+        this.gameObject.transform.position = camPosCopy;
+
+    }
+}
---
+++ Assets/Scripts/Scrolling/water_scrolling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water_scrolling : MonoBehaviour
+{
+
+    public int preparationTime;
+    public float waterSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new WaitForSeconds(preparationTime);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 waterPosition = this.gameObject.transform.position;
+        Vector3 waterScale = this.gameObject.transform.localScale;
+        
+        waterScale.y += waterSpeed * Time.deltaTime;
+        waterPosition.y += 0.5f * waterSpeed * Time.deltaTime;
+        this.gameObject.transform.position = waterPosition;
+        this.gameObject.transform.localScale = waterScale;
+
+    }
+}
