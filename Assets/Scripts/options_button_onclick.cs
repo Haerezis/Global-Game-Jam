edc79171cf965d30b8e3fd73b1e0f22d6cf0adcc
Kentigern <kentigern.hartmann@ensiie.fr>
2019-01-26 05:15:23 +0100

    Adding empty scene and onClick script to handle Option menu

---
+++ Assets/Scripts/options_button_onclick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class options_button_onclick : MonoBehaviour
+{
+
+    public Button options;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        options.onClick.AddListener(OptionOnClick);
+    }
+
+    // Update is called once per frame
+    void OptionOnClick()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+}
